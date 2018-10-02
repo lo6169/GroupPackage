ea85d5fcb147de77ef6062afa0a26bb18f1941cf
lo6169 <lo6169@rit.edu>
2018-10-02 15:29:50 -0400

    Testing Individual Activity

---
+++ app/src/main/java/com/example/lo6169/myapplication/MainActivitylo6169.java
@@ -0,0 +1,13 @@
+package com.example.lo6169.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitylo6169 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activitylo6169);
+    }
+}
