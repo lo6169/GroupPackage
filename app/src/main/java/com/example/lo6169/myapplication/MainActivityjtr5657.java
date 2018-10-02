7dd006986e05518697c624d7b484572d4bc6f89d
jtr5657 <jtr5657@rit.edu>
2018-10-02 15:51:18 -0400

    Jesenia's

---
+++ app/src/main/java/com/example/lo6169/myapplication/MainActivityjtr5657.java
@@ -0,0 +1,14 @@
+package com.example.lo6169.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityjtr5657 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activityjtr5657);
+    }
+}
+//
